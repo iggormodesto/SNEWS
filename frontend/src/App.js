6ae31d7ded822236946a74be2edf2b14c2ffbd73
Igor Modesto <igor.modesto>
2018-10-30 18:00:56 -0300

    Footer e tentativa de conexão com o backend usando Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,41 +2,19 @@ import React, { Component } from 'react';
 import snews from './imagens/logo.png';
 import editar from './imagens/edit.svg';
 import excluir from './imagens/delete.svg';
+import axios from 'axios';
 import { 
   Button, 
   Modal, 
   ModalHeader, 
   ModalBody, 
-  ModalFooter,
   Form,
   FormGroup, 
-  Label, 
   Input, 
-  FormText
 } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
-    Contatos = {
-      _embedded: {
-        contacts: []
-      },
-      page: {}
-    }
-
-  get() {
-    fetch('http://localhost:8080/contacts')
-    //.then(async (response) => {
-    // return response.json();
-    //})
-    //.then (async (r) =>  {
-    //  this.Contatos._embedded = r._embedded;
-    //  this.Contatos.page = r.page;
-    //})
-    //.catch((error) => {
-    //  console.error(error);
-    //});
-  }
 
   constructor(props) {
     super(props)
@@ -44,22 +22,23 @@ class App extends Component {
     	items: [
         { 
           id: "01",
-          nome: "Leonardo Pereira", 
-          genero: "Homem", 
-          nascimento: "18/06/1994", 
+          name: "Leonardo Pereira", 
+          gender: "Homem", 
+          birthday: "18/06/1994", 
           email: "leonardopereira@daurep.com",
-          telefone: "95 3760-3464" 
+          phone: "95 3760-3464" 
         },
         { 
           id: "02",
-          nome: "Igor Modesto", 
-          genero: "Homem", 
-          nascimento: "18/06/1994", 
+          name: "Igor Modesto", 
+          gender: "Homem", 
+          birthday: "18/06/1994", 
           email: "iggormoreira@hotmail.com.br",
-          telefone: "61 98100-7179" 
+          phone: "61 98100-7179" 
         }
       ],
-      modal: false
+      modal: false,
+      _embedded: []
     };
     this.toggle = this.toggle.bind(this);
   }
@@ -70,8 +49,15 @@ class App extends Component {
     });
   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:8080/contacts`)
+      .then(res => {
+        const _embedded = res.data;
+        this.setState({_embedded});
+      })
+  }
+
   render() {
-    this.get ();
     //{console.log(this.Contatos)}
     return (
       <div className="App">
@@ -98,14 +84,14 @@ class App extends Component {
                   <Form>
                     <FormGroup>
                       <Input type="text" name="name" id="name" placeholder="Nome" />
-                      <Input type="select" name="select" id="genero">
+                      <Input type="select" name="select" id="gender">
                         <option>Homem</option>
                         <option>Mulher</option>
                         <option>Não-binário</option>
                       </Input>
-                      <Input type="date" name="datetime" id="aniversario" placeholder="Aniversário" />
+                      <Input type="date" name="datetime" id="birthday" placeholder="Aniversário" />
                       <Input type="email" name="email" id="email" placeholder="E-mail" />
-                      <Input type="text" name="telefone" id="telefone" placeholder="Telefone" />
+                      <Input type="text" name="telefone" id="phone" placeholder="Telefone" />
                       <Button color="primary" onClick={this.toggle}>Salvar</Button>{' '}
                     </FormGroup>
                   </Form>
@@ -119,11 +105,11 @@ class App extends Component {
               <li className="row">
 
                 <div className="col">
-                  <p>{item.id} - {item.genero}</p>
-                  <h3>{item.nome}</h3>
-                  <p>{item.nascimento}</p>
+                  <p>{item.id} - {item.gender}</p>
+                  <h3>{item.name}</h3>
+                  <p>{item.birthday}</p>
                   <p>{item.email}</p>
-                  <p>{item.telefone}</p>
+                  <p>{item.phone}</p>
                 </div>
 
                 <div className="col-3 acoes">
@@ -139,10 +125,17 @@ class App extends Component {
             ))}
           </ul>
 
+          {/*<ul>
+            {this.state._embedded.map(_embedded => (
+              <li>{_embedded.contacts.name}</li>
+            ))}
+          </ul>
+          */}
+
         </section>
 
         <footer>
-          Footer
+          <p>Desenvolvido por <a href="https://www.linkedin.com/in/igor-modesto/" target="_blank">Igor Modesto</a></p>
         </footer>
       </div>
     );
